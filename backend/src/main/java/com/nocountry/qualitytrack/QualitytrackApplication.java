13ce94e3172d485484f7df0ebd25fdd09d22d5f5
judith2x <judivm86@gmail.com>
2026-09-05 18:38:52 -0600

    chore(backend): initialize Spring Boot project

---
+++ backend/src/main/java/com/nocountry/qualitytrack/QualitytrackApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.qualitytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitytrackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nocountry/qualitytrack/QualitytrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.qualitytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitytrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
